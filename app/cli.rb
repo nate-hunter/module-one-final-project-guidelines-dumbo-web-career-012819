e98ccaa3f9880fa98837826b55eb30c59aee3a47
github username <github email address>
2019-02-12 16:39:55 -0500

    added scenario 4 day 2.3

--- app/cli.rb
+++ app/cli.rb
@@ -6,38 +6,39 @@ def run_method
   prompt = TTY::Prompt.new
 
 # WELCOME MESSAGE & LOGIN #
-  ## 1. Displays welcome message;
-  ## 2. user enters username;
-  ## 3. user chooses to (1) start new game, (2) View highscore, and (3)...
-  banner
-  space
-  greeting
-  enter_name
-  user_name = gets.chomp
-  current_user = find_or_add_user(user_name)
-  space
-  element = prompt.select('What is your element?', ["Water", "Earth", "Air"])
-  current_user.update(element: element)
-  space
-  space
+  # 1. Displays welcome message;
+  # 2. user enters username;
+  # 3. user chooses to (1) start new game, (2) View highscore, and (3)...
+#   banner
+#   space
+#   greeting
+#   enter_name
+#   user_name = gets.chomp
+#   current_user = find_or_add_user(user_name)
+#   space
+#   element = prompt.select('What is your element?', ["Water", "Earth", "Air"])
+#   current_user.update(element: element)
+#   space
+#   space
 
-# GAME ONE INTRO #
-  ## 1. Introduces scenario
-  ## 2. Gives user option to investigate
-  intro
-  scene_one_answer1 = prompt.select('What do you do?', ["Go investigate", "Go back to sleep"])
-  space
+# # GAME ONE INTRO #
+#   ## 1. Introduces scenario
+#   ## 2. Gives user option to investigate
+#   intro
+#   scene_one_answer1 = prompt.select('What do you do?', ["Go investigate", "Go back to sleep"])
+#   space
 
-# FIRST SCENARIO #
-  ## 1. Option to engage fire-nation soldier
-  ## 2. Option to (1) direct attack, (2) sneak attack, or (3) sneak by
-  first_scenario_one_question(scene_one_answer1)
-  space
-  scene_one_answer2 = prompt.select('Do you engage?', ["Yes", "No"])
-  space
-  second_scenario_one_question(scene_one_answer2)
-  # FIGHT #
-  rsp_match_one(current_user)
+# # FIRST SCENARIO #
+#   ## 1. Option to engage fire-nation soldier
+#   ## 2. Option to (1) direct attack, (2) sneak attack, or (3) sneak by
+#   first_scenario_one_question(scene_one_answer1)
+#   space
+#   scene_one_answer2 = prompt.select('Do you engage?', ["Yes", "No"])
+#   space
+#   second_scenario_one_question(scene_one_answer2)
+#   # FIGHT #
+#   rsp_match_one(current_user)
+fourth_scenario
 
 
 
---
+++ app/game_one/04_fourth_scenario.rb
@@ -0,0 +1,572 @@
+def fourth_scenario
+
+  ##################################
+  ############ STRINGS #############
+  ##################################
+
+  def break_door_lock
+    system "clear"
+    puts "You find a metal pipe nearby and try to break the lock. You smash it a few times but it doesn’t break."
+  end
+
+  def kick_down_door
+    system "clear"
+    puts "You take a few steps back and try to kick the door down. It shakes the door but it doesn’t seem to be working."
+  end
+
+  def find_another_way
+    system "clear"
+    puts "You go around the temple in search of another way in. You find a closed window right around the corner."
+
+    find_another_way_prompt
+  end
+
+  def go_around
+    system "clear"
+    puts "The window seems too high to climb. You continue looking for a more suitable entrance. As you run around the temple, you find an open side door. You go inside. The temple is filled with smoke and it’s hard to see, but you can still hear the villagers."
+
+    follow_sounds_prompt
+  end
+
+  def break_window
+    system "clear"
+    puts "You look around and find a large rock. You throw the rock and it breaks the window. With a running start, you climb the wall and hang on the window ledge. You pull yourself up and make your way into the temple. Finally inside, you notice the temple is filled with smoke and it’s hard to see, but you can still hear the villagers."
+    
+    follow_sounds_prompt
+  end
+
+  def follow_sounds
+    system "clear"
+    puts "You call out for the villagers and their screams become louder. You follow their cries and find them huddled in a corner."
+
+    villagers_scared_prompt
+  end
+
+  def burning_pillar
+    system "clear"
+    puts "You tell the villagers to stay where they are and you’ll find a safe way to leave the temple. They’re hesitant but agree to stay put. You go back the way you came, but all of a sudden, you feel a rumble. Out of nowhere a burning pillar falls from the ceiling. You have 3 seconds to respond"
+    
+    burning_pillar_prompt
+  end
+
+  def burning_pillar_with_villagers
+    system "clear"
+    puts "You tell the villagers to hold hands and follow you. You lead them back the way you came, but all of a sudden, you feel a rumble. Out of nowhere a burning pillar falls from the ceiling. You have 3 seconds to respond"
+    
+    burning_pillar_with_villagers_prompt
+  end
+
+  def jump
+    system "clear"
+    puts "You jump backwards and the pillar crashes in front of you. The way you came has been blocked. You turn around and search for another exit. At that moment, you see a glimmer of sunlight in the corner."
+
+    jump_prompt
+  end
+
+  # def jump_with_villagers
+  #   system "clear"
+    
+  # end
+
+  def push_villagers
+    system "clear"
+    puts "You push the villagers out of the way and the pillar crashes between you and the villagers."
+    
+    push_villagers_prompt
+  end
+
+  def block
+    system "clear"
+    puts "You block the pillar from falling on you with your bending. The pillar is so heavy you strain yourself, but you manage to move it over to the side, preventing it from blocking the exit."
+
+    block_prompt
+  end
+
+  def block_with_villagers
+    system "clear"
+    puts "You block the pillar with your bending and hold it up. You yell at the villagers to quickly move under the pillar and out to safety. You’re strained beyond belief but somehow manage to keep the pillar from falling on top of the villagers as they scurry outside."
+
+    outside
+    
+    # block_with_villagers_prompt
+  end
+
+  def move_pillar
+    system "clear"
+    puts "You try to move the pillar with your bending, but it’s too heavy. You call to the villagers and tell them to stay where they are. You try to find a way around, but it’s hopeless."
+    
+    move_pillar_prompt
+  end
+
+  def give_up
+    system "clear"
+    puts "The task at hand is too monumental for you. You decide the best course of action is to abandon the villagers and save yourself. You run towards the exit and make it outside. Once you catch your breath, you continue going through the village."
+  end
+
+  def back_to_villagers
+    system "clear"
+    puts "You go back to look for the villagers. You find them hiding in the same corner."
+
+    back_to_villagers_prompt
+  end
+
+  def check_exit
+    system "clear"
+    puts "You decide you should check if there are any other surprises along the way. Fortunately, there aren't any and the way out seems clear."
+    
+    check_exit_prompt
+  end
+
+  def tell_villagers_to_follow
+    system "clear"
+    puts "You tell the villagers you found a way out and tell them to follow you. They eagerly get up and follow you outside."
+
+    outside
+  end
+
+  def destroy_pillar
+    system "clear"
+    puts "Unable to move the pillar, you try to destroy it instead. You gather all your remaining strength and attack the pillar. A few seconds later the pillar is destroyed and you can finally guide the villagers to safety."
+
+    destroy_pillar_prompt
+  end
+
+  def sunlight
+    system "clear"
+    puts "You follow the sunlight and find a door. You try opening it but it’s locked shut."
+
+    sunlight_prompt
+  end
+
+  def go_outside
+    system "clear"
+    puts "You finally make it outside with the villagers."
+
+    outside
+  end
+
+  def kick_door
+    system "clear"
+    puts "You kick the door a few times and it opens. You finally make it out of the temple."
+
+    outside
+  end
+
+  def tackle_door
+    system "clear"
+    puts "You tackle the door with your shoulder a few times but it doesn’t budge."
+
+    tackle_door_prompt
+  end
+
+  def tackle_door_again
+    system "clear"
+    puts "You tackle the door one last time and dislocate your shoulder."
+
+    tackle_door_again_prompt
+  end
+
+  def accept_fate
+    system "clear"
+    puts "Exhausted, injured, and unmotivated, you give up and sit down with your back against the door. You think back to your childhood and question what decisions you had to make to get here. As you start to doze off, you think to yourself, 'This isn't so bad'. You give into the fumes and the heat and give up."
+  end
+
+  def outside
+    system "clear"
+    puts "Once outside with all the villagers, they celebrate and thank you for saving them. As a token of their gratitude, they give you some bread and gloves. You thank them for the gifts and continue your way through the village."
+  end
+
+  ##################################
+  ############# START ##############
+  ##################################
+
+  prompt = TTY::Prompt.new
+  system "clear"
+
+  puts "You continue making your way through the village and come across a burning temple. You can hear people yelling for help inside."
+
+  intro_prompt_answer = prompt.select('What do you do?', ["Run inside and save them", "Look for water to put out the fire", "Walk away"])
+
+  ##################################
+  ########### ARGUMENTS ############
+  ##################################
+
+  def intro_prompt(intro_prompt_answer)
+
+    if intro_prompt_answer == "Run inside and save them"
+      system "clear"
+      puts "You run up the steps to save the villages. The doors have been chained shut."
+
+      run_into_temple
+
+    elsif intro_prompt_answer == "Look for water to put out the fire"
+      system "clear"
+      puts "You frantically search for water but find nothing. You return to the temple."
+
+      look_for_water
+
+    else
+      system "clear"
+      puts "It’s too dangerous and you need to save your energy in case you run into more trouble. You continue making your way through the village."
+    end
+  end
+
+
+  def run_into_temple
+    prompt = TTY::Prompt.new
+    run_into_temple_answer = prompt.select('What do you do?', ["Try to break the lock", "Kick down the door", "Look for another way in"])
+
+    run_into_temple_prompt(run_into_temple_answer)
+    
+  end
+
+  def run_into_temple_prompt(run_into_temple_answer)
+    
+    if run_into_temple_answer == "Try to break the lock"
+      break_door_lock
+      break_temple_lock_prompt
+
+    elsif run_into_temple_answer == "Kick down the door"
+      kick_down_door
+      kick_temple_door_prompt
+
+    else
+      find_another_way
+    end
+  end
+
+  def look_for_water
+    prompt = TTY::Prompt.new
+    intro_prompt_answer = prompt.select('What do you do?', ["Run inside and save them", "Walk away"])
+
+    intro_prompt(intro_prompt_answer)
+  end
+
+  ##################################
+  ##################################
+  ##################################
+
+  def break_temple_lock(break_temple_lock_answer)
+    
+    if break_temple_lock_answer == "Continue trying"
+      system "clear"
+      puts "You can’t give up so soon. With all your might you give the lock one final smash, and it breaks open. You kick the door open and run inside. The temple is filled with smoke and it’s hard to see, but you can still hear the villagers."
+
+      follow_sounds_prompt
+
+    elsif break_temple_lock_answer == "Kick down the door"
+      kick_down_door
+      kick_temple_door_prompt
+
+    else
+      find_another_way
+    end
+  end
+
+  def kick_temple_door(kick_temple_door_answer)
+
+    if kick_temple_door_answer == "Continue trying"
+      system "clear"
+      puts "You can’t give up so soon. You take a few steps back, run up and try to break the door. It doesn’t budge and you’ve hurt yourself in the process. You give up trying to kick the door down."
+
+      kick_temple_door_again_prompt
+
+    elsif kick_temple_door_answer == "Try to break the lock"
+      break_door_lock
+      break_temple_lock_prompt
+
+    else
+      find_another_way
+    end
+  end
+
+  def kick_temple_door_again_method(kick_temple_door_again_answer)
+    if kick_temple_door_again_answer == "Try to break the lock"
+      break_door_lock
+      break_temple_lock_after_kick_prompt
+
+    else
+      find_another_way
+    end
+  end
+
+  def find_another_way_method(find_another_way_answer)
+    if find_another_way_answer == "Continue searching for another way in"
+      go_around
+
+    else
+      break_window
+    end
+  end
+
+  ##################################
+  ##################################
+  ##################################
+
+  def break_temple_lock_prompt
+    prompt = TTY::Prompt.new
+    break_temple_lock_answer = prompt.select('What do you do?', ["Continue trying", "Kick down the door", "Look for another way in"])
+
+    break_temple_lock(break_temple_lock_answer)
+  end
+
+  def break_temple_lock_after_kick_prompt
+    prompt = TTY::Prompt.new
+    break_temple_lock_answer = prompt.select('What do you do?', ["Continue trying", "Look for another way in"])
+
+    break_temple_lock(break_temple_lock_answer)
+  end
+
+  def kick_temple_door_prompt
+    prompt = TTY::Prompt.new
+    kick_temple_door_answer = prompt.select('What do you do?', ["Continue trying", "Try to break the lock", "Look for another way in"])
+
+    kick_temple_door(kick_temple_door_answer)
+  end
+
+  def kick_temple_door_again_prompt
+    prompt = TTY::Prompt.new
+    kick_temple_door_again_answer = prompt.select('What do you do?', ["Try to break the lock", "Look for another way in"])
+
+    kick_temple_door_again_method(kick_temple_door_again_answer)
+  end
+
+  def find_another_way_prompt
+    prompt = TTY::Prompt.new
+    find_another_way_answer = prompt.select('What do you do?', ["Continue searching for another way in", "Break the window and climb in"])
+
+    find_another_way_method(find_another_way_answer)
+  end
+
+
+
+
+  ##################################
+  ############ PART II #############
+  ##################################
+
+
+  def follow_sounds_method(follow_sounds_answer)
+    follow_sounds
+  end
+
+  def villagers_scared_method(villagers_scared_answer)
+    if villagers_scared_answer == "Tell them to follow you"
+      burning_pillar_with_villagers
+    else
+      burning_pillar
+    end
+  end
+
+  def burning_pillar_method(burning_pillar_answer)
+    
+    if burning_pillar_answer == "Jump out of the way"
+      jump
+    else 
+      block
+    end
+  end
+
+  def burning_pillar_with_villagers_method(burning_pillar_with_villagers_answer)
+    
+    if burning_pillar_with_villagers_answer == "Jump out of the way"
+      jump
+
+    elsif burning_pillar_with_villagers_answer == "Push the villagers out of the way"
+      push_villagers
+
+    else
+      block_with_villagers
+    end
+
+  end
+
+  def jump_method(jump_answer)
+    if jump_answer == "Follow the sunlight"
+      sunlight
+    else
+      look_for_another_way
+    end
+  end
+
+  def push_villagers_method(push_villagers_answer)
+    if push_villagers_answer == "Try to move the pillar with your bending"
+      move_pillar
+    else
+      give_up
+    end
+  end
+
+  def block_method(block_answer)
+    if block_answer == "Go back to the villagers"
+      back_to_villagers
+    else
+      check_exit
+    end
+  end
+
+  def move_pillar_method(move_pillar_answer)
+    if move_pillar_answer == "Destroy pillar"
+      destroy_pillar
+    else
+      give_up
+    end
+  end
+
+  def back_to_villagers_method(back_to_villagers_answer)
+    tell_villagers_to_follow
+  end
+
+  def check_exit_method(check_exit_answer)
+    if check_exit_answer == "Go back to the villagers"
+      back_to_villagers
+    else
+      give_up
+    end
+  end
+
+  def destroy_pillar_method(destroy_pillar_answer)
+    go_outside
+  end
+
+  def sunlight_method(sunlight_answer)
+    if sunlight_answer == "Kick the door"
+      kick_door
+    else
+      tackle_door
+    end
+  end
+
+  def tackle_door_method(tackle_door_answer)
+    if tackle_door_answer == "Continue trying"
+      tackle_door_again
+    else
+      kick_door
+    end
+  end
+
+  def tackle_door_again_method(tackle_door_again_answer)
+    if tackle_door_again_answer == "Accept your fate"
+      accept_fate
+    else
+      kick_door
+    end
+  end
+
+  # def block_with_villagers_method(block_with_villagers_answer)
+    
+  # end
+
+  ##################################
+  ##################################
+  ##################################
+
+  def follow_sounds_prompt
+    prompt = TTY::Prompt.new
+    follow_sounds_answer = prompt.select('What do you do?', ["Follow the cries of the villagers"])
+
+    follow_sounds_method(follow_sounds_answer)
+  end
+
+  def villagers_scared_prompt
+    prompt = TTY::Prompt.new
+    villagers_scared_answer = prompt.select('What do you do?', ["Tell them to follow you", "Tell them to stay while you find a safe way out"])
+
+    villagers_scared_method(villagers_scared_answer)
+  end
+
+  def burning_pillar_prompt
+    prompt = TTY::Prompt.new
+    burning_pillar_answer = prompt.select('What do you do?', ["Jump out of the way", "Block the pillar with your bending"])
+
+    burning_pillar_method(burning_pillar_answer)
+  end
+
+  def burning_pillar_with_villagers_prompt
+    prompt = TTY::Prompt.new
+    burning_pillar_with_villagers_answer = prompt.select('What do you do?', ["Jump out of the way", "Push the villagers out of the way", "Block the pillar with your bending"])
+
+    burning_pillar_with_villagers_method(burning_pillar_with_villagers_answer)
+  end
+
+  def jump_prompt
+    prompt = TTY::Prompt.new
+    jump_answer = prompt.select('What do you do?', ["Follow the sunlight", "Look for another way out"])
+
+    jump_method(jump_answer)
+  end
+
+  def push_villagers_prompt
+    prompt = TTY::Prompt.new
+    push_villagers_answer = prompt.select('What do you do?', ["Try to move the pillar with your bending", "Give up and save yourself"])
+
+    push_villagers_method(push_villagers_answer)
+  end
+
+  def block_prompt
+    prompt = TTY::Prompt.new
+    block_answer = prompt.select('What do you do?', ["Go back to the villagers", "Check to see if the exit is clear"])
+
+    block_method(block_answer)
+  end
+
+  def move_pillar_prompt
+    prompt = TTY::Prompt.new
+    move_pillar_answer = prompt.select('What do you do?', ["Destroy pillar", "Give up and save yourself"])
+
+    move_pillar_method(move_pillar_answer)
+  end
+
+  def back_to_villagers_prompt
+    prompt = TTY::Prompt.new
+    back_to_villagers_answer = prompt.select('What do you do?', ["Tell the villagers to come with you"])
+
+    back_to_villagers_method(back_to_villagers_answer)
+  end
+
+  def check_exit_prompt
+    prompt = TTY::Prompt.new
+    check_exit_answer = prompt.select('What do you do?', ["Go back to the villagers", "Go outside and save yourself"])
+
+    check_exit_method(check_exit_answer)
+  end
+
+  def destroy_pillar_prompt
+    prompt = TTY::Prompt.new
+    destroy_pillar_answer = prompt.select('What do you do?', ["Go outside"])
+
+    destroy_pillar_method(destroy_pillar_answer)
+  end
+
+  def sunlight_prompt
+    prompt = TTY::Prompt.new
+    sunlight_answer = prompt.select('What do you do?', ["Kick the door", "Tackle the door"])
+
+    sunlight_method(sunlight_answer)
+  end
+
+  def tackle_door_prompt
+    prompt = TTY::Prompt.new
+    tackle_door_answer = prompt.select('What do you do?', ["Kick the door", "Continue trying"])
+
+    tackle_door_method(tackle_door_answer)
+  end
+
+  def tackle_door_again_prompt
+    prompt = TTY::Prompt.new
+    tackle_door_again_answer = prompt.select('What do you do?', ["Kick the door", "Accept your fate"])
+
+    tackle_door_again_method(tackle_door_again_answer)
+  end
+
+  # def block_with_villagers_prompt
+  #   prompt = TTY::Prompt.new
+  #   block_with_villagers_answer = prompt.select('What do you do?', ["Follow the sunlight", "Look for another way out"])
+
+  #   block_with_villagers_method(block_with_villagers_answer)
+  # end
+
+
+  intro_prompt(intro_prompt_answer)
+
+end
